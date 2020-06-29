fc877c250cb4ebba278b3197d73d8c2bbb2af860
C411XM3XN00B <30630624+C411XM3XN00B@users.noreply.github.com>
2020-06-29 22:23:28 +0200

    Upload of Java version
    Uploaded a java version of the sine array generator to generate arrays faster and without uploading to an arduino.

---
+++ SINE_ARRAY_GENERATOR/sine_array_generator.java
@@ -0,0 +1,36 @@
+/**
+ * sine_array_generator
+ */
+import java.lang.Math.*;
+
+public class sine_array_generator {
+
+    public static void main(String[] args) {
+        
+        
+
+        int arrayLength = 8;        //should be divisible by 4 to get peaks
+        int maxValue = 1023;        //2^(number of bits the DAC has) - 1 
+
+
+        int valOffset = (maxValue + 1) / 2;
+         int valAmplitude = (valOffset - 1);
+
+        int val = 0;
+
+        System.out.print("byte sineValues[] = {");
+        val = valOffset;
+        System.out.print(val);
+        for (int i = 1; i < arrayLength; i++) {
+            System.out.print(", ");
+            val = (int) Math.round((valOffset + (valAmplitude * Math.sin((2* Math.PI * i)/arrayLength))));
+            System.out.print(val);
+          }
+        System.out.println("};");
+
+
+
+
+
+    }
+}
